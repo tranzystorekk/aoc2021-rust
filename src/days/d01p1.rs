c845ab54a38d3646538a5f5a6dc5f3b76e7a7ce2
Marcin Puc <marcin.e.puc@gmail.com>
2021-12-01 06:34:59 +0100

    Add solution for d01p1

--- src/days/d01p1.rs
+++ src/days/d01p1.rs
@@ -1,13 +1,24 @@
+use std::io::BufRead;
+
 use aoc_utils::BufferedInput;
+use itertools::Itertools;
 
 #[anyhoo::anyhoo]
-fn parse_input() {
+fn parse_input() -> Vec<i32> {
+    let input = BufferedInput::parse_args("")?;
 
+    input.lines().map_ok(|l| l.parse().unwrap()).try_collect()?
 }
 
 #[anyhoo::anyhoo]
 fn main() {
     let input = parse_input()?;
 
-
+    aoc_utils::measure_and_print(|| {
+        input
+            .into_iter()
+            .tuple_windows()
+            .filter(|(a, b)| a < b)
+            .count()
+    })
 }
