1c8a5d82fceb1e9e08c60538b4531474a3ccdada
Marcin Puc <marcin.e.puc@gmail.com>
2021-12-12 11:09:24 +0100

    Add solutions for day 12

---
+++ src/days/d12p1.rs
@@ -0,0 +1,67 @@
+use std::collections::{HashMap, HashSet};
+
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+use scan_fmt::scan_fmt;
+
+#[anyhoo::anyhoo]
+fn parse_input() -> Tunnels {
+    let input = BufferedInput::parse_args("Day 12: Passage Pathing - Part 1")?;
+
+    input
+        .unwrapped_lines()
+        .flat_map(|l| {
+            let (a, b) = scan_fmt!(&l, "{}-{}", String, String).unwrap();
+
+            if a == "start" {
+                return vec![(a, b)];
+            }
+
+            if b == "start" {
+                return vec![(b, a)];
+            }
+
+            vec![(a.clone(), b.clone()), (b, a)]
+        })
+        .into_group_map()
+}
+
+type Tunnels = HashMap<String, Vec<String>>;
+
+fn is_small(cave: &str) -> bool {
+    cave.chars().all(|c| c.is_ascii_lowercase())
+}
+
+fn sweep_paths(tunnels: &Tunnels) -> usize {
+    let mut searchspace: Vec<(&str, HashSet<&str>)> = vec![("start", ["start"].into())];
+    let mut result = 0;
+
+    while let Some((curr, small_visited)) = searchspace.pop() {
+        if curr == "end" {
+            result += 1;
+            continue;
+        }
+
+        for neighbor in &tunnels[curr] {
+            if !is_small(neighbor) {
+                searchspace.push((neighbor, small_visited.clone()));
+                continue;
+            }
+
+            let mut new_visited = small_visited.clone();
+
+            if new_visited.insert(neighbor) {
+                searchspace.push((neighbor, new_visited));
+            }
+        }
+    }
+
+    result
+}
+
+#[anyhoo::anyhoo]
+fn main() {
+    let input = parse_input()?;
+
+    aoc_utils::measure_and_print(|| sweep_paths(&input));
+}
---
+++ src/days/d12p2.rs
@@ -0,0 +1,74 @@
+use std::collections::{HashMap, HashSet};
+
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+use scan_fmt::scan_fmt;
+
+#[anyhoo::anyhoo]
+fn parse_input() -> Tunnels {
+    let input = BufferedInput::parse_args("Day 12: Passage Pathing - Part 2")?;
+
+    input
+        .unwrapped_lines()
+        .flat_map(|l| {
+            let (a, b) = scan_fmt!(&l, "{}-{}", String, String).unwrap();
+
+            if a == "start" {
+                return vec![(a, b)];
+            }
+
+            if b == "start" {
+                return vec![(b, a)];
+            }
+
+            vec![(a.clone(), b.clone()), (b, a)]
+        })
+        .into_group_map()
+}
+
+type Tunnels = HashMap<String, Vec<String>>;
+
+fn is_small(cave: &str) -> bool {
+    cave.chars().all(|c| c.is_ascii_lowercase())
+}
+
+fn sweep_paths(tunnels: &Tunnels) -> usize {
+    let mut searchspace: Vec<(&str, HashSet<&str>, bool)> = vec![("start", [].into(), false)];
+    let mut result = 0;
+
+    while let Some((curr, small_visits, double_visit)) = searchspace.pop() {
+        if curr == "end" {
+            result += 1;
+            continue;
+        }
+
+        for neighbor in &tunnels[curr] {
+            if !is_small(neighbor) {
+                searchspace.push((neighbor, small_visits.clone(), double_visit));
+                continue;
+            }
+
+            let mut new_visits = small_visits.clone();
+
+            if !double_visit {
+                let is_second_visit = !new_visits.insert(neighbor);
+
+                searchspace.push((neighbor, new_visits, is_second_visit));
+                continue;
+            }
+
+            if new_visits.insert(neighbor) {
+                searchspace.push((neighbor, new_visits, double_visit));
+            }
+        }
+    }
+
+    result
+}
+
+#[anyhoo::anyhoo]
+fn main() {
+    let input = parse_input()?;
+
+    aoc_utils::measure_and_print(|| sweep_paths(&input));
+}
